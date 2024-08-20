4665ff43bf763b147bd7eb986eb065361059f0f9
hubert <hubert@inexa-sys-pc2.inexa-ci.lan>
2024-08-20 19:37:39 +0200

    cicd file5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Je m'appelle hubert et je suis le plus beau de la terre  Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
